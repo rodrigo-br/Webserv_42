0e9253d6cac8171259fe4dcd92e1d54221bcbb8e
larissa <larissa_silva@outlook.com>
2023-08-08 18:15:53 -0300

    Feat: created first version of server with multiple ports

--- includes/classes/Server.hpp
+++ includes/classes/Server.hpp
@@ -13,6 +13,11 @@ class Server
         Conf conf;
         Server();
         static void signalHandler(int signum);
+
+        void run();
+        std::vector<int> listen_sockets;
+        fd_set current_sockets;
+        int max_socket_so_far;
 };
 
 #endif
