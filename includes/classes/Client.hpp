6d55206960d5dcb0b55d12046d8fd51a9271e838
larissa <larissa_silva@outlook.com>
2023-08-11 16:01:25 -0300

    WIP: Added the instance of the Request Validator and Request class in the loop

---
+++ includes/classes/Client.hpp
@@ -0,0 +1,24 @@
+// #ifndef CLIENT_H
+// # define CLIENT_H
+
+// # include <map>
+// # include <string>
+// # include "classes/Location.hpp"
+// # include "classes/ConfParser.hpp"
+// # include "classes/ServerData.hpp"
+
+// class Client
+// {
+//     private:
+//         int                 _client_socket;
+//         struct sockaddr_in  _client_address;
+
+//     public:
+//         Client();
+//         ~Client();
+//         const int                 &getSocket() const;
+//         const struct sockaddr_in  &getAddress() const;
+
+// };
+
+// #endif
