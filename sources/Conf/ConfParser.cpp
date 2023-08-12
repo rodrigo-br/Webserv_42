95e003c208469a252851f132ef5fbca9bdbb1a18
rodrigo-br <rodrigoab123@gmail.com>
2023-08-12 15:24:49 -0300

    more refactoring

--- sources/Conf/ConfParser.cpp
+++ sources/Conf/ConfParser.cpp
@@ -1,6 +1,33 @@
 #include "classes/ConfParser.hpp"
 #include <sstream>
 
+static bool notEmptyLineAndFailed(size_t size, bool succeed)
+{
+    return (size > 0) && !succeed;
+}
+
+static bool isServerBlock(std::vector<std::string> tokens)
+{
+    return (tokens[0].compare("server") == 0) && (tokens[1].compare("{") == 0);
+}
+
+static bool isLocationBlock(std::vector<std::string> tokens)
+{
+    // Need to verify token[1] for a valid location path
+    return (tokens[0].compare("location") == 0 && tokens[2].compare("{") == 0);
+}
+
+static bool switchMe(bool &me)
+{
+    me = !me;
+    return me;
+}
+
+// static bool invalidTokensSize(size_t size)
+// {
+//     return (size > 3) || (size <= 0);
+// }
+
 ConfParser::ConfParser(std::string file) :
     _succeed(false), _inServerBrackets(false), _inLocationBrackets(false)
 {
@@ -39,23 +66,12 @@ void ConfParser::createServers()
                 token = std::strtok(NULL, " \n");
             }
             this->_succeed = assignTokens(tokens);
-            if (tokens.size() > 0 && !this->_succeed)
+            if (notEmptyLineAndFailed(tokens.size(), this->_succeed))
             {
                 break ;
             }
         }
     }
-}
-
-static bool isServerBlock(std::vector<std::string> tokens)
-{
-    return (tokens[0].compare("server") == 0) && (tokens[1].compare("{") == 0);
-}
-
-static bool isLocationBlock(std::vector<std::string> tokens)
-{
-    // Need to verify token[1] for a valid location path
-    return (tokens[0].compare("location") == 0 && tokens[2].compare("{") == 0);
 }
 
 bool ConfParser::isValidClosingBracket(std::string token)
@@ -79,33 +95,26 @@ bool ConfParser::isValidClosingBracket(std::string token)
 bool ConfParser::assignTokens(std::vector<std::string> tokens)
 {
     size_t tokensSize = tokens.size();
-    if (tokensSize > 3 || tokensSize <= 0)
+    switch(tokensSize)
     {
-        return false;
+        case 1:
+            return isValidClosingBracket(tokens[0]);
+        case 2:
+            if (isServerBlock(tokens))
+            {
+                return switchMe(this->_inServerBrackets);
+            }
+            // Need to check for valid configurations
+            return (tokens[0].compare("location") != 0);
+        case 3:
+            if (isLocationBlock(tokens))
+            {
+                return switchMe(this->_inLocationBrackets);
+            }
+            return false;
+        default:
+            return false;
     }
-    else if (tokensSize == 1)
-    {
-        return isValidClosingBracket(tokens[0]);
-    }
-    else if (tokensSize == 2)
-    {
-        if (isServerBlock(tokens))
-        {
-            this->_inServerBrackets = !this->_inServerBrackets;
-            return this->_inServerBrackets;
-        }
-        // Need to check for valid configurations
-        return (tokens[0].compare("location") != 0);
-    }
-    else if (tokensSize == 3)
-    {
-        if (isLocationBlock(tokens))
-        {
-            this->_inLocationBrackets = !this->_inLocationBrackets;
-            return this->_inLocationBrackets;
-        }
-    }
-    return false;
 }
 
 bool ConfParser::succeed()
