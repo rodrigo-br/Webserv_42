3577f39e19515bfdbaebb6856d88100526370d37
larissa <larissa_silva@outlook.com>
2023-08-13 16:57:45 -0300

    refactor: added updates

--- includes/classes/Server.hpp
+++ includes/classes/Server.hpp
@@ -2,7 +2,6 @@
 # define SERVER_HPP
 
 # include "webserv.hpp"
-#include  "classes/Socket.hpp"
 
 # define BUFFER_SIZE 30000
 
@@ -14,14 +13,9 @@ class Server
         Server(Conf &config);
         static void signalHandler(int signum);
 
-        bool initializeSockets();
         void runServer(Socket socket);
-        void setClients();
         std::vector<int> listenSockets;
         std::vector<int> clienstSocks;
-        fd_set read_fds;
-        fd_set write_fds;
-
 };
 
 #endif
