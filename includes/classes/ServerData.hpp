54f1309ee3c18ac31159e107af3ded42916f5be4
rodrigo-br <rodrigoab123@gmail.com>
2023-09-05 10:13:05 -0300

    bodySizeLimit

--- includes/classes/ServerData.hpp
+++ includes/classes/ServerData.hpp
@@ -11,26 +11,29 @@ class ServerData
 {
     private:
         std::string _root;
-        std::map<std::string, Location> _location;
-        std::map<std::string, void (ServerData::*)(std::string)> _configurations;
+        int _bodySizeLimit;
         std::string _currentLocation;
         std::vector<std::string> _serverNames;
+        std::map<std::string, Location> _location;
+        std::map<std::string, void (ServerData::*)(std::string)> _configurations;
 
     public:
         ServerData();
 
         std::string getRoot() const;
-        std::string getLocation(std::string locationName) const;
+        int getBodySizeLimit() const;
         std::vector<std::string> getServerNames() const;
         std::map<std::string, Location> getLocations() const;
+        std::string getLocation(std::string locationName) const;
         void setRoot(std::string root);
+        void setBodySizeLimit(std::string value);
+        void setLocationIndex(std::string index);
+        void setServerNames(std::string directoryListening);
         void setLocation(std::string path, Location location);
         void setConfiguration(std::vector<std::string> tokens);
-        void setLocationIndex(std::string index);
+        bool isDirectoryListingLocation(std::string locationPath);
         void setLocationAllowedMethods(std::string allowedMethods);
         void setLocationDirectoryListening(std::string directoryListening);
-        void setServerNames(std::string directoryListening);
-        bool isDirectoryListingLocation(std::string locationPath);
 };
 
 #endif
