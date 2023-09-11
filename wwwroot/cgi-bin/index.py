4e97453323c685655a0dd8cf5fb3c801893b87f5
rodrigo-br <rodrigoab123@gmail.com>
2023-09-11 12:06:14 -0300

    cgi temp deleting file and returning 500 for syntax errors

--- wwwroot/cgi-bin/index.py
+++ wwwroot/cgi-bin/index.py
@@ -9,10 +9,13 @@ print()
 sys.stdout.reconfigure(encoding='utf-8')
 
 # Gera a página HTML
-print("<html>")
+prin("<html>")
 print("<head><title>Script CGI Python</title></head>")
 print("<body>")
 print("<h1>Ola, humano!</h1>")
 print("<p>Eu sou um script CGI Python simples.</p>")
 print("</body>")
 print("</html>")
+
+while(True):
+    continue
